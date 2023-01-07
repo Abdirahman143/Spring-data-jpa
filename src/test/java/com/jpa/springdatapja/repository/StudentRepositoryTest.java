90ca5893d2db86fb820b13b012fb1de3914b58a1
Abdi <KEN20963@kcbgroup.com>
2023-01-07 09:46:56 +0300

    getting all the student data in the database on test

--- src/test/java/com/jpa/springdatapja/repository/StudentRepositoryTest.java
+++ src/test/java/com/jpa/springdatapja/repository/StudentRepositoryTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -26,6 +28,12 @@ class StudentRepositoryTest {
         studentRepository.save(student);
     }
 
+    @Test
+public void getAllStudent(){
+        List<Student> studentList = studentRepository.findAll();
+        System.out.println("studentList = " + studentList);
+
+}
 
 
 }
